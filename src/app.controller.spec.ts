60e7bfab4999bdc04c7912d49a86f2d29582a9cd
Frisky Fox <zrebec@zrebec.sk>
2023-07-03 18:44:55 +0200

    fix: test to strict equal

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,9 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toEqual({ message: 'Hello, world' });
+      expect(appController.getHello()).toStrictEqual({
+        message: 'Hello, world',
+      });
     });
   });
 });
